29e599d5b7d431c6dcd83b8bc00a8b70dd9a5c9c
Felix Kapfer <wi20177@lehre.dhbw-stuttgart.de>
2021-12-04 21:42:36 +0100

    Fehlerbehebung

--- src/cgi-bin/website/auth.py
+++ src/cgi-bin/website/auth.py
@@ -4,21 +4,21 @@ from website.user import User
 auth = Blueprint('auth', __name__)
 
 @auth.route('/sign-up')
-def register():
+def auth_register():
     return render_template('auth/sign-up.html')
     
 @auth.route('/sign-in')
-def login():
+def auth_login():
     return render_template('auth/sign-in.html')
 
 @auth.route('/registerd-successfull/')
-def registerdSuccessfull():
+def auth_registerdSuccessfull():
 
     flash('User was registerd successfull - You are now able to log in with your credentials', 'alert-success')
     return render_template('auth/sign-in.html', flashArea='TEST')
 
 @auth.route('/logout')
-def logout():
+def auth_logout():
     user = User()
     result = user.logout()
     flash('Log out was successfull!')
